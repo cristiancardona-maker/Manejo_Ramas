431cc71a249962a0d3f7a93ec1758aa3d1c6a885
Cristian Cardona <cristian.cardona@estudiantesunibague.edu.co>
2026-08-14 13:24:05 -0500

    Recibe nombre d elos usuarios y los guarda

--- usuarios.py
+++ usuarios.py
@@ -1,1 +1,6 @@
-cuantos = int(input("Cuantos usuarios van a crear"))
+cuantos = int(input("Cuantos usuarios van a crear?"))
+
+usuarios = []
+for i in range(cuantos):
+    nombre = input("Ingrese el nombre del usuario")
+    usuarios.append(nombre)
